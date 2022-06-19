97586d3dada8d08c0c42dc436b407abbda4d58bb
C201 <97812356+CRD201@users.noreply.github.com>
2022-06-19 13:06:32 +0200

    Version2 and fixes

--- main.cpp
+++ main.cpp
@@ -1,76 +1,100 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
-#include <utility>
-#include <algorithm>
-#include <vector>
-#include <string>
 
-#include "board.hpp"
-#include "dummy.hpp"
+#include "versions.hpp"
 
 int main()
 {
+    /// Setting up window
     sf::RenderWindow window(sf::VideoMode(1185, 1000), L"OBRAZKOWE UKŁADANKI", sf::Style::Titlebar | sf::Style::Close);
     window.setPosition(sf::Vector2i(40, 40));
     window.setFramerateLimit(30);
     sf::Event event;
     sf::Color color = sf::Color::Black;
 
+    /// Choosing random image
     std::random_device dev;
     std::mt19937 rng(dev());
     std::uniform_int_distribution<std::mt19937::result_type> numberOfImage(1,4);
     std::string path = "../img/img" + std::to_string(numberOfImage(rng)) + ".png";
     std::string test_img{path};
 
-    Board* board = new Dummy(test_img);
+
+    /// Setting up menu text
+    sf::Text text, title;
+    sf::Font font;
+
+    font.loadFromFile("../img/OpenSans-Bold.ttf");
+    title.setFont(font);
+    title.setCharacterSize(40);
+    title.setFillColor(sf::Color::White);
+    text.setFont(font);
+    text.setCharacterSize(30); 
+    text.setFillColor(sf::Color::White);
+
+    title.setPosition(400, 400);
+    text.setPosition(400, 460);
+    title.setString(L"Wybór wersji:");
+    text.setString("1 - wersja z zamienianiem \n2 - wersja z przesuwaniem");
+
+    /// Setting up board and game version variables
+    Board* board = nullptr;
+    Game_version game_version = Game_version::None;
 
     while (window.isOpen())
     {
         window.clear(color);
 
-        sf::Text text;
-        sf::Font font;
-        font.loadFromFile("../img/OpenSans-Bold.ttf");
-        text.setFont(font);
-        text.setCharacterSize(20); 
-        text.setFillColor(sf::Color::White);
-        text.setPosition(100,925);
-        text.setString("1 - wersja z zamienianiem, 2 - wersja z przesuwaniem");
-        window.draw(text);
-
+        /// event handling
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed) 
                 window.close();
 
-            else if(event.type == sf::Event::KeyPressed)
+            else if (event.type == sf::Event::KeyPressed)
             {
-                if(event.key.code == sf::Keyboard::Tab){
-                    board->switch_mode();
+                if (game_version != Game_version::None){
+                    if (event.key.code == sf::Keyboard::Tab){
+                        board->switch_mode();
+                    }
                 }
+                else {
+                    if (event.key.code == sf::Keyboard::Num1){
+                        board = new Version1(test_img);
+                        game_version = Game_version::First;
+                        board->scramble();
+                    }
 
-                if (event.key.code == sf::Keyboard::Num1 || event.key.code == sf::Keyboard::Num2){
-                    board->setGameMode(event.key.code);
-                    board->scramble();
-                }
-                
-                if(event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left){
-                    board->move(sf::Vector2i(event.mouseButton.x, event.mouseButton.y));
+                    if (event.key.code == sf::Keyboard::Num2){
+                        board = new Version2(test_img);
+                        game_version = Game_version::Secund;
+                        board->scramble();
+                    }
                 }
             }
             
-        if(event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left){
-            board->move(sf::Vector2i(event.mouseButton.x, event.mouseButton.y));
+            if (game_version != Game_version::None)
+                if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left)
+                    board->move(sf::Vector2i(event.mouseButton.x, event.mouseButton.y));
         }
-        if (board->getGameMode() != 0)
+        
+        /// drowing and chacking win conditions
+        if (game_version != Game_version::None) {
             if(board->solved())
                 window.close();
-        }
 
-        window.draw(*board);
+            window.draw(*board);
+        }
+        else {
+            window.draw(title);
+            window.draw(text);
+        }
+        
+        /// displaying content on window
         window.display();
     }
 
-    delete board;
+    if(board != nullptr)
+        delete board;
 
 }
